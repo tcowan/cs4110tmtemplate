7cc172322d1a0c56c0e4db7c396c3d20348f5807
Ted Cowan <tcowan@my321.net>
2021-08-24 00:51:43 -0600

    apparently the log command doesn't work in support scripts; added cucumber.yml

--- features/support/grading.rb
+++ features/support/grading.rb
@@ -1,17 +1,17 @@
 $total_points = 0
 
 Before do
-	# log Dir.pwd
+	# puts Dir.pwd
 	ENV['LIBC_FATAL_STDERR_'] = "1"
 end
 
 at_exit do
-	log "A total of #{$total_points} points have been awarded."
+	puts "A total of #{$total_points} points have been awarded."
 	running = `ps` 
 	if running =~ /wsh/
-		log "*** one or more wsh processes are still running"
-		log running
-		log "*** Issuing \"killall wsh\""
+		puts "*** one or more wsh processes are still running"
+		puts running
+		puts "*** Issuing \"killall wsh\""
 		`killall wsh`
 	end
 end
